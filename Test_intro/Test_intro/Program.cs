17556bdb6117452b862dee898270934211e75d94
ellisju37073 <67281488+ellisju37073@users.noreply.github.com>
2024-09-23 08:19:37 -0700

    Initital
    Initial

---
+++ Test_intro/Test_intro/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Test_intro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
